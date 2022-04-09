020ea123055ad4b04ec326e60b53497d56fc4793
correavitor4 <correavitor4@hotmail.com>
2022-04-09 18:15:06 -0300

    .

---
+++ components/HomeMap/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const HomeMap = () =>{
+    return(
+        <View style={{height:300, backgroundColor:"#a0abff"}}>
+            <Text>I am map</Text>
+        </View>
+    )
+}
---
+++ screens/HomeScreen/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const HomeScreen = () =>{
+    return(
+        <View>
+            <Text>Just a Text</Text>
+        </View>
+    )
+    
+}
+export default HomeScreen
